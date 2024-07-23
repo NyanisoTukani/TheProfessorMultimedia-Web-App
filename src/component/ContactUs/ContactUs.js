3b366c36d044bc785d3e8c9d63c25a4f8d24ddf9
NyanisoTukani <222954690@tut4life.ac.za>
2024-07-23 10:48:50 +0200

    Made the form to work

--- src/component/ContactUs/ContactUs.js
+++ src/component/ContactUs/ContactUs.js
@@ -1,9 +1,44 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import emailjs from '@emailjs/browser';
 import './ContactUs.css'
-export class ContactUs extends Component{
+const ContactUs = () => {
 
-    render(){
+    const [fname,setName]  = useState('');
+    const [email,setEmail]  = useState('');
+    const [phoneNumber,setPhoneNumber]  = useState('');
+    const [message,setMessage] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const serviceId = 'service_jewalaa';
+        const templateId = 'template_tkj1y49';
+        const publicKey = 'SfLigHbzXLVl4R9PU';
+
+        const templateParam = {
+            from_name:fname,
+            from_email:email,
+            from_phoneNumber:phoneNumber,
+            to_name:'Nyaniso',
+            message:message,
+        };
+
+        //Send email using my EmailJs
+
+        emailjs.send(serviceId,templateId,templateParam,publicKey)
+               .then((response) => {
+                    alert('Email sent Successfully', response);
+                    setName('');
+                    setEmail('');
+                    setPhoneNumber('');
+                    setMessage('');
+               })
+               .catch((error) => {
+                    alert.error('Error sending email', error);
+               });
+    }
+
+   
         return(
 
             <>
@@ -15,17 +50,16 @@ export class ContactUs extends Component{
                 <div className='contactUs-form-container'>
                     <div className='contactUs-sub-heading'><h1>Want to work with us or have a question?</h1></div>
                     <div className='contact-us-form'>
-                        <form className='form-inputs'>
-                            <div className='inputs'><input type='text' placeholder='Your Name....' className='textfield' name='name' /></div>
-                            <div className='inputs'><input type='text' placeholder='Your Email....' className='textfield' name='email' /></div>
-                            <div className='inputs'><input type='text' placeholder='Your Phone Number....' className='textfield' name='phonenumber' /></div>
-                            <div className='inputs'><textarea  className='textArea' placeholder='Write a message....' name ='message' /></div>
+                        <form onSubmit={handleSubmit} className='form-inputs'>
+                            <div className='inputs'><input type='text' placeholder='Your Name....' className='textfield' value={fname} onChange={(e) => setName(e.target.value)} /></div>
+                            <div className='inputs'><input type='text' placeholder='Your Email....' className='textfield' value={email} onChange={(e) => setEmail(e.target.value)} /></div>
+                            <div className='inputs'><input type='text' placeholder='Your Phone Number....' className='textfield' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} /></div>
+                            <div className='inputs'><textarea  className='textArea' placeholder='Write a message....' value={message}  onChange={(e) => setMessage(e.target.value)}  /></div>
                             <div className='buttons'><button type='submit'>Submit</button></div>
                         </form>
                     </div>
                 </div>
             </>
         )
-    }
 }
 export default ContactUs
